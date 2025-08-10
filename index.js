b1b6f92578844b3b52656bb3467f96bb3fa23ecd
Gitansh Maski <maskigitansh633@gmail.com>
2025-08-10 17:11:00 +0530

    fix .env

--- index.js
+++ index.js
@@ -8,14 +8,6 @@ import mongoose from 'mongoose';
 const app = express();
 const port = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('✅ Connected to MongoDB');
-}).catch(err => {
-  console.error('❌ MongoDB connection error:', err);
-});
 
 app.use(cors());
 app.use(express.json());
